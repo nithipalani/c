5af0add969bd23f286f0ba8cf3e3c57a18e1b98a
nithipalani <48514126+nithipalani@users.noreply.github.com>
2019-03-13 16:18:29 +0530

    Create set_1_1.c

---
+++ set_1_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i;
+scanf("%d",&i);
+if(i==0)
+{
+printf("the number is zero");
+}
+if(i>0)
+{
+printf("the number is posstive");
+}
+if(i<0)
+{
+printf("the number is negnative");
+}
+
+return 0;
+}
